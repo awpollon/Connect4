d9ad416d3c74a4e39d383798fdf9f33ef3035815
awp1121 <awpollon@gmail.com>
2014-11-04 00:50:37 -0500

    add mvc, add displayPiece method

--- connect4.js
+++ connect4.js
@@ -2,11 +2,25 @@
  * @author Aaron Pollon
  */
 
-$(document).ready( function(){
-	$('tr').click(function() {
-		$(this).addClass('red-piece');
-	});	
-	
+$(document).ready(function() {
+	view.displayPiece('00', true);
 });
 
+var model = {
+	redTurn : true,
+	board : []
+
+};
+
+var view = {
+	displayPiece: function(location, isRed) {
+		var playerClass = 'red-piece';
+		if(!isRed) playerClass = 'black-piece';
+		$('#' + location).addClass(playerClass);
+	}
+};
+
+var controller = {
+	
+};
 
