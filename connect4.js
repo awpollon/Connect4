7e251331348b699f425efcbee233671c2dd3d148
awp1121 <awpollon@gmail.com>
2014-11-04 01:25:39 -0500

    more

--- connect4.js
+++ connect4.js
@@ -4,6 +4,9 @@
 
 $(document).ready(function() {
 	view.displayPiece('00', true);
+	view.setMsg('Testing msg');
+	
+	init();
 });
 
 var model = {
@@ -13,14 +16,42 @@ var model = {
 };
 
 var view = {
-	displayPiece: function(location, isRed) {
+	displayPiece: function(location) {
 		var playerClass = 'red-piece';
-		if(!isRed) playerClass = 'black-piece';
+		if(!model.redTurn) playerClass = 'black-piece';
 		$('#' + location).addClass(playerClass);
+	},
+	
+	setMsg: function(msg) {
+		$('#msg').text(msg);
 	}
 };
 
 var controller = {
+	endTurn: function() {
+		
+		this.updateTurn();
+	},
 	
+	checkForWin: function() {
+		
+	},
+	
+	
+	updateTurn: function() {
+		//Toggle the turn
+		model.redTurn = !model.redTurn;
+		if(model.redTurn) {
+			view.setMsg('Red\'s Turn');
+		}
+		else view.setMsg('Black\'s Turn');
+	}
 };
+
+function init() {
+	$('td').click(function() {
+			view.displayPiece(this.id);
+			controller.endTurn();
+		});
+}
 
