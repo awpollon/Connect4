ee9215fc4f52346d696dc189c819bc188788ed6f
awp1121 <awpollon@gmail.com>
2014-11-06 00:23:44 -0500

    begin setting up game logic, controller methods

--- connect4.js
+++ connect4.js
@@ -3,57 +3,102 @@
  */
 
 $(document).ready(function() {
-	view.displayPiece('00', true);
-	view.setMsg('Testing msg');
-	
+	//Test code
+	// view.displayPiece('00', true);
+	// view.setMsg('Testing msg');
+	//End test code
+
 	init();
 });
 
 var model = {
-	redTurn : true,
+	gameActive: true,
+	redTurn : true, //Red always goes first. Update turn method assumes this
+	currentPlayer : "Red",
 	board : [[]],
 	numCols : 7,
-	numRows: 6
+	numRows : 6,
+	numPiecesPlayed : 0 //Number of pieces in the game (divide by 2  and floow to get turn number)
 
 };
-//Comment
+
 var view = {
-	displayPiece: function(location) {
+	displayPiece : function(location) {
 		var playerClass = 'red-piece';
-		if(!model.redTurn) playerClass = 'black-piece';
+		if (!model.redTurn)
+			playerClass = 'black-piece';
 		$('#' + location).addClass(playerClass);
 	},
-	
-	setMsg: function(msg) {
+
+	setMsg : function(msg) {
 		$('#msg').text(msg);
 	}
 };
 
 var controller = {
-	endTurn: function() {
-		
-		this.updateTurn();
+	endTurn : function() {
+		//Increment number of pieces played
+		model.numPiecesPlayed++;
+
+		//Check for connect 4
+		if (this.checkForWin()) {
+			//Highlight winning pieces (using view method)
+			//Display winning messege (using view method)
+			view.setMsg(model.currentPlayer + " Wins!");
+			//End game (controller method)
+		}
+
+		//If not connect4, check if board is full
+		else if (this.isBoardFull()) {
+			//Print draw messege
+			view.setMsg("Board is full. It's a draw!");
+			//End game
+		} else {
+			//If game isn't over, switch to other player
+			this.updateTurn();
+			//Print turn msg
+			view.setMsg(model.currentPlayer + '\'s turn.');
+		}
 	},
-	
-	checkForWin: function() {
-		
+
+	checkForWin : function() {
+		//Scan game board for 4 in a row, return true if foudn
+
+		//Delete me
+		return false;
 	},
-	
-	
-	updateTurn: function() {
+
+	isBoardFull : function() {
+		console.log("numPiecesPlayed: " + model.numPiecesPlayed);
+		return ((model.numPiecesPlayed) === (model.numCols * model.numRows));
+	},
+
+	updateTurn : function() {
 		//Toggle the turn
 		model.redTurn = !model.redTurn;
-		if(model.redTurn) {
-			view.setMsg('Red\'s Turn');
+		if (model.redTurn) {
+			model.currentPlayer = "Red";
+		} else {
+			model.currentPlayer = "Black";
 		}
-		else view.setMsg('Black\'s Turn');
 	}
 };
 
 function init() {
+	//Set welcome messege
+	view.setMsg("Welcome to Connect 4! Red goes first. Enjoy!");
+	
+	//Handle table cell click
 	$('td').click(function() {
+		
+		//Check that game has not endede
+		if (model.gameActive) {
+			//Display a piece at that location (for testing)
 			view.displayPiece(this.id);
+			//End the turn
 			controller.endTurn();
-		});
+		}
+		//Else game is over, do nothing
+	});
 }
 
