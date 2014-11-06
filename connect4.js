9feb24a7fd0272a24803dc1ed0d2df948f48be86
awp1121 <awpollon@gmail.com>
2014-11-06 00:41:52 -0500

    create move object, other improvements

--- connect4.js
+++ connect4.js
@@ -12,14 +12,17 @@ $(document).ready(function() {
 });
 
 var model = {
-	gameActive: true,
+	gameActive : true,
 	redTurn : true, //Red always goes first. Update turn method assumes this
 	currentPlayer : "Red",
 	board : [[]],
 	numCols : 7,
 	numRows : 6,
-	numPiecesPlayed : 0 //Number of pieces in the game (divide by 2  and floow to get turn number)
+	numPiecesPlayed : 0, //Number of pieces in the game (divide by 2  and floow to get turn number)
 
+	validMove: function(cellID) {
+		//Check
+	}
 };
 
 var view = {
@@ -87,18 +90,41 @@ var controller = {
 function init() {
 	//Set welcome messege
 	view.setMsg("Welcome to Connect 4! Red goes first. Enjoy!");
-	
+
 	//Handle table cell click
 	$('td').click(function() {
-		
+
 		//Check that game has not endede
 		if (model.gameActive) {
 			//Display a piece at that location (for testing)
-			view.displayPiece(this.id);
-			//End the turn
-			controller.endTurn();
+			// view.displayPiece(this.id);
+			
+			//Create new moveObj
+			var move = new moveObj(model.currentPlayer, this.id);
+			
+			//Check for valid move
+			if (model.validMove(move)) {
+				//Handle move
+				controller.handleMove(move);
+				
+				//End the turn
+				controller.endTurn();
+			}
+			else {
+				view.setMsg("That column is full. Please select another.");
+			}
+
 		}
 		//Else game is over, do nothing
 	});
 }
+
+//Object to store move information
+var moveObj = function(player, cellID){
+	this.player = player;
+	this.row = model.numRows - cellID[0] -1; //Flip row number so row 0 is at bottom
+	this.col = cellID[1];
+	console.log("Move: player: " + this.player + ", row: " + this.row + ", col: " + this.col);
+};
+
 
